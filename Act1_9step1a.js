ea01c04505c91e2f1d58cb542019b7bee7c24be0
pedwardsau56 <pedwards_sg@hotmail.com>
2023-07-21 10:59:45 +1000

    Exercise completed

---
+++ Act1_9step1a.js
@@ -0,0 +1,17 @@
+// The following function creates a greeting (you do not need to modify this statement)
+function addGreeting(message) {
+
+    // Create a constant variable called greeting to store the string "Hello! The message is: "
+    const greeting = "Hello! The message is: ";
+    
+    // Close the loop by using the return statement and using a value sustition that adds the greeting to the message
+    return `${greeting}${message}`;
+}
+// Create a constant variable to store this unit's code
+const unitCode = 'IFQ714';
+
+// Define a variable that stores the function for the greeting plus the unit code (you do not need to modify this statement)
+newMessage = addGreeting(unitCode);
+
+// Show the message in the console (you do not need to modify this statement)
+console.log(newMessage);
