ef0ddf9d6c231a0a9fecc7db612ff1a86c018e11
pedwardsau56 <pedwards_sg@hotmail.com>
2023-07-21 14:30:05 +1000

    cleanup

--- Act1_9step1a.js
+++ Act1_9step1a.js
@@ -1,13 +1,13 @@
 // The following function creates a greeting (you do not need to modify this statement)
-function addGreeting(message) {
-
-    // Task: Create a constant variable called greeting to store the string "Hello! The message is: "
+function addGreeting(message)
+{
+    // Task: Create a constant value called greeting to store the string "Hello! The message is: "
     const greeting = "Hello! The message is: ";
     
-    // Close the loop using the return statement to perform a value sustition that adds the greeting to the message (the return statement hasn't been used in the course yet)
+    // Close the loop using the return statement to perform a value substition that adds the greeting to the message (the return statement hasn't been used in the course yet)
     return `${greeting}${message}`;
 }
-// Task: Create a constant variable to store this unit's code
+// Task: Create a constant value called unitCode to store this unit's code
 const unitCode = 'IFQ714';
 
 // Define a variable that stores the function for the greeting plus the unit code (you do not need to modify this statement)
