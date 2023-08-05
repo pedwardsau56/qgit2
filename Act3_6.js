877b321064739f661a88e1ddf3a6ff500ce4e9d2
pedwardsau56 <pedwards_sg@hotmail.com>
2023-08-05 16:38:14 +1000

    constructor

--- Act3_6.js
+++ Act3_6.js
@@ -1,11 +1,4 @@
-// Example from https://stackoverflow.com/questions/9991805/javascript-how-to-parse-json-array
-// var myMessage = '{"success": true,"counters": [{"counter_name": "dsd", "counter_type": "sds", "counter_unit": "sds" }, { "counter_name": "gdg", "counter_type": "dfd", "counter_unit": "ds" }, { "counter_name": "sdsData", "counter_type": "sds", "counter_unit": " dd " }, { "counter_name": "Stoc final", "counter_type": "number ", "counter_unit": "litri " }, { "counter_name": "Consum GPL", "counter_type": "number ", "counter_unit": "litri " }, { "counter_name": "sdg", "counter_type": "dfg", "counter_unit": "gfgd" }, { "counter_name": "dfgd", "counter_type": "fgf", "counter_unit": "liggtggggri " }, { "counter_name": "fgd", "counter_type": "dfg", "counter_unit": "kwfgf " }, { "counter_name": "dfg", "counter_type": "dfg", "counter_unit": "dg" }, { "counter_name": "gd", "counter_type": "dfg", "counter_unit": "dfg" } ] }';
-// var jsonData = JSON.parse(myMessage);
-// for (var i = 0; i < jsonData.counters.length; i++) {
-//    var counter = jsonData.counters[i];
-//    console.log(counter.counter_name);
-// }
-//===================================================================================
+
 // Step 1: Array of objects - employees and sales
 const employees = [{"id":1,"firstName":"John","lastName":"Smith","gender":"Male","age":23,"position":"Manager"},
 {"id":2,"firstName":"Mary","lastName":"Sue","gender":"Female","age":32,"position":"Salesperson"},
@@ -22,3 +15,26 @@ const sales = [{"staffId":1,"item":"Wi-Fi Adapter","price":40.00,"date":"01-09-2
 {"staffId":3,"item":"Pre-built PC","price":1999.95,"date":"02-09-2022"},
 {"staffId":3,"item":"USB Cable","price":5.00,"date":"02-09-2022"},
 {"staffId":3,"item":"HDMI Cable","price":15.45,"date":"02-09-2022"}]
+//===================================================================================
+// Step 2 display employee details
+function showEmployeeInfo(employees) {
+           console.log(`Name: ${employees.firstName} ${employees.lastName}\nStaff ID: ${employees.id}\nGender: ${employees.gender}\nAge: ${employees.age}\nPosition: ${employees.position}\n`);
+    }
+
+ // Function to traverse all the employees and show information for each
+ function showAllEmployeesInfo() {
+    for (let i = 0; i < employees.length; i++) {
+       showEmployeeInfo(employees[i]);
+    }
+ }
+ showAllEmployeesInfo();
+//===================================================================================
+// Step 3: Create a constructor
+function employee(id,firstName,lastName,gender,age,position) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.age = age;
+    this.position = position;
+}
